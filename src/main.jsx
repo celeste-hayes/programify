676839be43f8c0859e9695a2b15dace5ab66528a
Celeste Hayes <hayes.celeste93@gmail.com>
2025-01-04 09:47:17 -0500

    Chore: reorganize page tree update missing files and folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './pages/home.jsx';
