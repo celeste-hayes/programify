f81da50424be4b6d7317344e7a18953d085100cf
Celeste Hayes <hayes.celeste93@gmail.com>
2025-01-10 09:25:21 -0500

    Update Vite config to bind to the correct port

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: true,                 
+    port: process.env.PORT || 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5175'
-       
+        target: 'http://localhost:5175',
       },
     },
   },
