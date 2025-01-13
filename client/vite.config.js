23770bf11a3752531938df9bd2039fcbfcc20728
RyDeveloper13 <orvials88@gmail.com>
2025-01-13 18:59:30 -0500

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,23 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     host: true,
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 3001,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
-      // WebSocket proxy configuration
-      '/socket.io': {
-        target: 'http://localhost:3001', // Backend WebSocket server port
-        ws: true,  // This is important for WebSocket connections
-      },
-      '/auth': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false
-      },
+      
     },
   },
   plugins: [react()],
