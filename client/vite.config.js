f1936d6aecf7c5ef538e9111de4f9c0415977698
Celeste Hayes <hayes.celeste93@gmail.com>
2025-01-13 20:03:23 -0500

    Render deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,30 +1,35 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+
+const port = process.env.PORT || 3000;
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,  // Port for the Vite dev server
-    open: true,  // Automatically open the browser when the server starts
+    port,
+    open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001', // Proxy API requests to this target
-        changeOrigin: true,  // This is useful if the target server uses different origins
-        secure: false,       // Disable SSL verification for the proxy (useful for dev)
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
       },
       '/auth': {
-        target: 'http://localhost:3001', // Proxy /auth requests to this target
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       },
     },
     hmr: {
-      protocol: 'ws',  // Use WebSocket for HMR (important if you're using http or https)
-      host: 'localhost',  // Ensure the correct host for WebSocket connections
-      port: 3001, // The same port as your Vite dev server
-      clientPort: 3000, // The port that the client connects on (defaults to 3000)
-      timeout: 30000, // Optional: Set timeout for HMR to avoid issues in slow networks
+      protocol: 'ws',
+      host: 'localhost',
+      port: 3001,
+      clientPort: port,
+      timeout: 30000,
     },
   },
 });
+
+
+
 
